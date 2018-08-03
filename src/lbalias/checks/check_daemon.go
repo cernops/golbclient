94dd839c1eec676778cb7b6302fe37073bcd4722
Paulo Canilho <paulo.canilho@cern.ch>
2018-08-03 13:29:50 +0200

    V2.1 - WIP: Implemented the generic Daemon check LB-95

--- src/lbalias/checks/check_daemon.go
+++ src/lbalias/checks/check_daemon.go
@@ -4,20 +4,50 @@ import (
 	"bufio"
 	"fmt"
 	"lbalias/utils/logger"
+	"lbalias/utils/parser"
 	"os"
 	"regexp"
+	"strings"
 )
 
 type DaemonListening struct {
 	code int
-	port int
 }
 
 func (daemon DaemonListening) Code() int {
 	return daemon.code
 }
 
-func (daemon DaemonListening) daemonListen(proc string) bool {
+// daemonEntry : helper struct used to parse the metric line
+type daemonEntry struct {
+	prot protocol
+	ipv  ipLevel
+	port int
+}
+
+// protocol : int type used to distinguish between different transport protocol types
+type protocol int
+
+// ipLevel : int type used to distinguish between different IP levels
+type ipLevel int
+
+// Enum for the transport protocol types supported
+const (
+	TCPUDP protocol = iota
+	TCP
+	UDP
+)
+
+// Enum for the transport protocol types supported
+const (
+	IPV4V6 ipLevel = iota
+	IPV4
+	IPV6
+	ANY
+)
+
+// isListening : checks if the given proc/daemon is running on the required port
+func (daemon DaemonListening) scanNetwork(proc string, port int) bool {
 	file, err := os.Open(proc)
 	if err != nil {
 		logger.Error("Error opening [%s]", proc)
@@ -28,7 +58,7 @@ func (daemon DaemonListening) daemonListen(proc string) bool {
 	scanner := bufio.NewScanner(file)
 	// The format of the file is 'sl  local_address rem_address   st'
 
-	portHex := fmt.Sprintf("%04x", daemon.port)
+	portHex := fmt.Sprintf("%04x", port)
 
 	logger.Debug("Scanning port [%s]", portHex)
 	portOpen, _ := regexp.Compile("^ *[0-9]+: [0-9A-F]+:" + portHex + " [0-9A-F]+:[0-9A-F]+ 0A")
@@ -43,28 +73,161 @@ func (daemon DaemonListening) daemonListen(proc string) bool {
 }
 
 func (daemon DaemonListening) Run(args ...interface{}) interface{} {
-	rVal := false
-	if (daemon.port < 1) || (daemon.port > 65535) {
-		return false
-	}
-	listen := []string{}
-	if daemon.daemonListen("/proc/net/tcp") {
-		listen = append(listen, "ipv4")
-		rVal = true
-	}
-	if daemon.daemonListen("/proc/net/tcp6") {
-		listen = append(listen, "ipv6")
-		rVal = true
+	// Process the daemon metric
+	entry, err := daemon.processDaemonMetric(args[0].(string))
+	if err != nil {
+		return daemon.code
 	}
 
-	if logger.GetLevel() == logger.DEBUG {
-		if len(listen) > 0 {
-			for _, p := range listen {
-				logger.Debug("Daemon [%s] on port [%d] is listening", p, daemon.port)
-			}
-		} else {
-			logger.Debug("Daemon is not listening in port [%d]", daemon.port)
-		}
+	// Check if the given port is within bounds
+	if (entry.port < 1) || (entry.port > 65535) {
+		logger.Error("The specified port is out of range [1-65535]. Failing with code [%d]", daemon.code)
+		return false
 	}
-	return rVal
+	return daemon.isListening(entry.prot, entry.ipv, entry.port)
+}
+
+// isListening : checks if a daemon is listening on the given protocol(s) in the selected IP level and port
+func (daemon DaemonListening) isListening(prot protocol, ipv ipLevel, port int) bool {
+	switch prot {
+	case TCP:
+		switch ipv {
+		case IPV4:
+			if daemon.scanNetwork("/proc/net/tcp", port) {
+				logger.Debug("The daemon is listening on the transport protocol type [tcp/v4] with the port [%d]", port)
+				return true
+			}
+			logger.Debug("The daemon is not listening on the transport protocol type [tcp/v4] with the port [%d]", port)
+		case IPV6:
+			if daemon.scanNetwork("/proc/net/tcp6", port) {
+				logger.Debug("The daemon is listening on the transport protocol type [tcp/v6] with the port [%d]", port)
+				return true
+			}
+			logger.Debug("The daemon is not listening on the transport protocol type [tcp/v6] with the port [%d]", port)
+		case IPV4V6:
+			if daemon.scanNetwork("/proc/net/tcp", port) && daemon.scanNetwork("/proc/net/tcp6", port) {
+				logger.Debug("The daemon is listening on the transport protocol type [tcp/all] with the port [%d]", port)
+				return true
+			}
+			logger.Debug("The daemon is not listening on the transport protocol type [tcp/all] with the port [%d]", port)
+		case ANY:
+			if daemon.scanNetwork("/proc/net/tcp", port) || daemon.scanNetwork("/proc/net/tcp6", port) {
+				logger.Debug("The daemon is listening on the transport protocol type [tcp] with the port [%d]", port)
+				return true
+			}
+			logger.Debug("The daemon is not listening on the transport protocol type [tcp] with the port [%d]", port)
+		}
+	case UDP:
+		switch ipv {
+		case IPV4:
+			if daemon.scanNetwork("/proc/net/udp", port) {
+				logger.Debug("The daemon is listening on the transport protocol type [udp/v4] with the port [%d]", port)
+				return true
+			}
+			logger.Debug("The daemon is not listening on the transport protocol type [udp/v4] with the port [%d]", port)
+		case IPV6:
+			if daemon.scanNetwork("/proc/net/udp6", port) {
+				logger.Debug("The daemon is listening on the transport protocol type [udp/v6] with the port [%d]", port)
+				return true
+			}
+			logger.Debug("The daemon is not listening on the transport protocol type [udp/v6] with the port [%d]", port)
+		case IPV4V6:
+			if daemon.scanNetwork("/proc/net/udp", port) && daemon.scanNetwork("/proc/net/udp6", port) {
+				logger.Debug("The daemon is listening on the transport protocol type [udp/all] with the port [%d]", port)
+				return true
+			}
+			logger.Debug("The daemon is not listening on the transport protocol type [udp/all] with the port [%d]", port)
+		case ANY:
+			if daemon.scanNetwork("/proc/net/udp", port) || daemon.scanNetwork("/proc/net/udp", port) {
+				logger.Debug("The daemon is listening on the transport protocol type [udp] with the port [%d]", port)
+				return true
+			} else {
+				logger.Debug("The daemon is not listening on the transport protocol type [udp] with the port [%d]", port)
+			}
+		}
+	case TCPUDP:
+		return daemon.isListening(TCP, ipv, port) && daemon.isListening(UDP, ipv, port)
+	}
+	return false
+}
+
+// processDaemonMetric : extracts the protocol, IP level and port from the given metric line
+func (daemon DaemonListening) processDaemonMetric(line string) (de daemonEntry, err error) {
+	// Log
+	logger.Debug("Processing [daemon] metric entry line [%s]", line)
+	found := regexp.MustCompile("(?i)((check)( )+(daemon))").Split(strings.TrimSpace(line), -1)
+	if len(found) != 2 {
+		// Log
+		logger.Error("Incorrect syntax specified at [%s]. Please use (`check` `daemon` `<protocol><ip_level (optional)>:<port>`). Failing with code [%d]", line, daemon.code)
+		return de, err
+	}
+
+	// Early port number syntax check
+	if !strings.Contains(line, ":") {
+		// Log
+		logger.Error("Incorrect syntax specified at [%s]. Please use (`check` `daemon` `<protocol><ip_level (optional)>:<port>`). Failing with code [%s]", line, daemon.code)
+		return de, err
+	}
+
+	rawMetricLine := found[1]
+	//Log
+	logger.Debug("Found metric line [%v]", rawMetricLine)
+
+	// If no expression was given, fail the whole expression
+	if len(strings.TrimSpace(rawMetricLine)) == 0 {
+		logger.Error("Detected a (check) metric-type without any given metric. Return [false]")
+		return de, err
+	}
+
+	// Discover the desired transport protocol & IP level (i.e. first set of paired squared parenthesis)
+	rawProtocol := regexp.MustCompile(`\[([^\[\]]*)\]`).FindAllString(rawMetricLine, -1)[0]
+	if len(strings.TrimSpace(rawProtocol)) == 2 {
+		logger.Error("Incorrect syntax was detected when processing the metric line [%s]. The desired transport protocol must be specified (e.g. udp/tcp/all)")
+		return de, err
+	}
+
+	/************************************************** Syntax checks ***********************************************/
+
+	// Discover if an IP version was given
+	ipVersion := regexp.MustCompile("/").Split(rawProtocol, -1)
+	if len(ipVersion) > 1 {
+		parsedLC := strings.ToLower(ipVersion[1])
+		if parsedLC == "v4" {
+			de.ipv = IPV4
+		} else if parsedLC == "v6" {
+			de.ipv = IPV6
+		} else {
+			// @TODO review syntax error failures in the application level
+			logger.Error("Syntax error detected when attempting to extract the IP level from the metric line [%s]. The value [%s] is not supported. Defaulting to [all]", line, parsedLC)
+			de.ipv = IPV4V6
+		}
+	} else {
+		// Check IPv4 or IPv6 if nothing was given
+		de.ipv = ANY
+	}
+
+	// Discover the type of transport protocol
+	runes := []rune(rawProtocol)
+	parsedLC := strings.ToLower(string(runes[1:4]))
+	if parsedLC == "udp" {
+		de.prot = UDP
+	} else if parsedLC == "tcp" {
+		de.prot = TCP
+	} else {
+		// @TODO review syntax error failures in the application level
+		logger.Error("Syntax error detected when attempting to extract the transport protocol from the metric line [%s]. The value [%s] is not supported. Defaulting to [all]", line, parsedLC)
+		de.prot = TCPUDP
+	}
+
+	// Discover the port number
+	rawPortNumber := regexp.MustCompile(`[\]][:]`).Split(line, 1)
+	if len(rawPortNumber) != 2 {
+		logger.Error("Syntax error detected when attempting to extract the port number from the metric line [%s]. The port number must be supplied using the ':' character at the end of your metric. Failing with code [%d]", line, daemon.code)
+		return de, err
+	}
+
+	// Assign port
+	de.port = int(parser.ParseInterfaceAsInteger(rawPortNumber[1]))
+
+	return de, nil
 }
