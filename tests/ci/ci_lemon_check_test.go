0efa60ec4a475abe958dbbbe4b4ee73f68e20814
Pablo Saiz <psaiz@cern.ch>
2019-04-08 10:13:01 +0200

    Tests for lemon

--- tests/ci/ci_lemon_check_test.go
+++ tests/ci/ci_lemon_check_test.go
@@ -1,12 +1,10 @@
 package ci
 
 import (
-	"gitlab.cern.ch/lb-experts/golbclient/lbconfig/mapping"
 	"strings"
 	"testing"
 
 	"gitlab.cern.ch/lb-experts/golbclient/helpers/logger"
-	"gitlab.cern.ch/lb-experts/golbclient/lbconfig"
 	"gitlab.cern.ch/lb-experts/golbclient/lbconfig/utils/runner"
 )
 
@@ -14,7 +12,7 @@ import (
 func TestCILemonCLI(t *testing.T) {
 	logger.SetLevel(logger.ERROR)
 	output, err := runner.Run("/usr/sbin/lemon-cli",
-		true , defaultTimeout, "--script", "-m", "13163")
+		true, defaultTimeout, "--script", "-m", "13163")
 	if err != nil {
 		logger.Error("An error was detected when running the CI [lemon-cli]")
 		t.FailNow()
@@ -25,50 +23,11 @@ func TestCILemonCLI(t *testing.T) {
 	logger.Trace("CI [lemon-cli] output [%s]", output)
 }
 
-// TestLemonFunctionality : fundamental functionality test for the [lemon-cli], output value must not be negative
-func TestLemonFunctionality(t *testing.T) {
-	logger.SetLevel(logger.ERROR)
-	cfg := mapping.NewConfiguration("../test/lbclient_lemon_check_single.conf", "myTest")
+func TestLemon(t *testing.T) {
+	var myTests [3]lbTest
+	myTests[0] = lbTest{"CollectdFunctionality", "../test/lbclient_lemon_check_single.conf", true, 12, nil, nil}
+	myTests[1] = lbTest{"ConfigurationFile", "../test/lbclient_lemon_check.conf", true, 8, nil, nil}
+	myTests[2] = lbTest{"LemonFailed", "../test/lbclient_lemon_check_fail", false, -11, nil, nil}
 
-	err := lbconfig.Evaluate(cfg, defaultTimeout)
-	if err != nil {
-		logger.Error("Detected an error when attempting to evaluate the alias [%s], Error [%s]", cfg.ConfigFilePath, err.Error())
-		t.Fail()
-	}
-	if cfg.MetricValue < 0 {
-		logger.Error("The metric output value returned negative [%d]. Failing the test...", cfg.MetricValue)
-		t.Fail()
-	}
-}
-
-// TestLemonConfigurationFile : integration test for all the functionality supplied by the lemon-cli
-func TestLemonConfigurationFile(t *testing.T) {
-	logger.SetLevel(logger.ERROR)
-
-	cfg := mapping.NewConfiguration("../test/lbclient_lemon_check.conf", "lemonTest")
-	err := lbconfig.Evaluate(cfg, defaultTimeout)
-	if err != nil {
-		logger.Error("Failed to run the client for the given configuration file [%s]. Error [%s]", cfg.ConfigFilePath, err.Error())
-		t.Fail()
-	}
-	if cfg.MetricValue < 0 {
-		logger.Error("The metric output value returned negative [%d]. Failing the test...", cfg.MetricValue)
-		t.Fail()
-	}
-}
-
-// TestLemonFailedConfigurationFile : integration test for all the functionality supplied by the lemon-cli, fail test
-func TestLemonFailedConfigurationFile(t *testing.T) {
-	logger.SetLevel(logger.ERROR)
-
-	cfg := mapping.NewConfiguration("../test/lbclient_lemon_check_fail.conf", "lemonFailTest")
-	err := lbconfig.Evaluate(cfg, defaultTimeout)
-	if err == nil {
-		logger.Error("Expecting an error for the given configuration file [%s]. Failing test...", cfg.ConfigFilePath)
-		t.Fail()
-	}
-	if cfg.MetricValue >= 0 {
-		logger.Error("The metric output value returned positive [%d] when expecting a negative output. Failing the test...", cfg.MetricValue)
-		t.Fail()
-	}
+	runMultipleTests(t, myTests[:])
 }
