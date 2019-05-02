b7be41de3d3e04abcbde62437b69a93e02e43c4b
Pablo Saiz <psaiz@cern.ch>
2019-05-02 10:02:02 +0200

    Patchint the attribute check

---
+++ tests/ci/ci_attribute_test.go
@@ -0,0 +1,15 @@
+package ci
+
+import (
+	"testing"
+)
+
+func TestAttributest(t *testing.T) {
+	myTests := []lbTest{
+		{title: "TestXsessions", configurationContent: "check xsessions\nload constant 34", expectedMetricValue: 34},
+		{title: "TestSwapping", configurationContent: "check swapping\nload constant 35", expectedMetricValue: 35},
+		{title: "TestAttributeBroken", configurationContent: "check bbswapping\nload constant 35", expectedMetricValue: -1, shouldFail: true},
+	}
+
+	runMultipleTests(t, myTests)
+}
