e3f860314db85a6d826b6e35657b54ff9b8292e2
Paulo Canilho <paulo.canilho@cern.ch>
2019-05-03 15:28:51 +0200

    Reverting

--- lbconfig/checks/check_attribute.go
+++ lbconfig/checks/check_attribute.go
@@ -1,9 +1,12 @@
 package checks
 
+import "gitlab.cern.ch/lb-experts/golbclient/helpers/logger"
+
 type CheckAttribute struct {
 }
 
 func (checkAttribute CheckAttribute) Run(...interface{}) (int, error) {
 	// This will be used later on for the default load
-	return -1, nil
+	logger.Trace("This check always returns true. It will be used to calculate the default load")
+	return 1, nil
 }
