7ddaa086d859d0be7db4e387283e59edd98e1fe5
Paulo Canilho <paulo.canilho@cern.ch>
2019-05-03 15:34:41 +0200

    Removing the future syntax check for metrics

--- tests/ci/ci_attribute_test.go
+++ tests/ci/ci_attribute_test.go
@@ -8,7 +8,8 @@ func TestAttributest(t *testing.T) {
 	myTests := []lbTest{
 		{title: "TestXsessions", configurationContent: "check xsessions\nload constant 34", expectedMetricValue: 34},
 		{title: "TestSwapping", configurationContent: "check swapping\nload constant 35", expectedMetricValue: 35},
-		{title: "TestAttributeBroken", configurationContent: "check swappingbbb\nload constant 35", expectedMetricValue: -1, shouldFail: true},
+		// @TODO add a syntax check
+		// {title: "TestAttributeBroken", configurationContent: "check swappingbbb\nload constant 35", expectedMetricValue: -1, shouldFail: true},
 	}
 
 	runMultipleTests(t, myTests)
