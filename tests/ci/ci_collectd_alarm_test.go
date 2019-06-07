081e52afc60e4e9eabe66b1263b416c4f1132708
Paulo Canilho <paulo.canilho@cern.ch>
2019-06-07 11:23:56 +0200

    Changing the collectd test name

--- tests/ci/ci_collectd_alarm_test.go
+++ tests/ci/ci_collectd_alarm_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestCollectdAlarms(t *testing.T) {
 	myTests := []lbTest{
-		{title: "CollectdFunctionality",
+		{title: "CollectdAlarmFunctionality",
 			configuration: "../test/lbclient_collectd_alarm_check_single.conf", expectedMetricValue: 2},
 	}
 
