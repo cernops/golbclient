1fe45d5aa3f942e1a6f54c1137586ff45dfe85f7
Paulo Canilho <paulo.alexandre.pereira.martins.canilho@cern.ch>
2019-08-21 13:13:55 +0000

    Check command now takes into account the existance of a file and its permissions alongside a command not found error

--- lbconfig/checks/check_command.go
+++ lbconfig/checks/check_command.go
@@ -2,8 +2,10 @@ package checks
 
 import (
 	"fmt"
+	"os/exec"
 	"regexp"
 	"strings"
+	"syscall"
 
 	"gitlab.cern.ch/lb-experts/golbclient/helpers/logger"
 	"gitlab.cern.ch/lb-experts/golbclient/lbconfig/utils/runner"
@@ -15,6 +17,11 @@ type Command struct{}
 	@TODO use the runner API to enable pipped commands support
 */
 
+const (
+	CommandNotFoundCode 	= 127
+	CommandNotExecutable 	= 126
+)
+
 func (command Command) Run(args ...interface{}) (int, error) {
 	cmd, _ := regexp.Compile("(?i)(^check[ ]+command)")
 	line := args[0].(string)
@@ -25,7 +32,20 @@ func (command Command) Run(args ...interface{}) (int, error) {
 		logger.Trace("Attempting to run command [%s]", usrCmd)
 		out, err := runner.RunCommand(usrCmd, true, 0)
 		if err != nil {
-			logger.Error("The command [%s] failed", usrCmd)
+			logger.Error("The command [%s] failed. Error [%v]", usrCmd, err)
+			if exitErr, ok := err.(*exec.ExitError); ok {
+				if exitErr.Exited() {
+					if status, ok := exitErr.Sys().(syscall.WaitStatus); ok {
+						if status.ExitStatus() == CommandNotFoundCode || status.ExitStatus() == CommandNotExecutable {
+							// If command not found or not executable
+							return -1, err
+						}
+					}
+					// If command exited with status != 0
+					return -1, nil
+				}
+			}
+
 			return -1, err
 		}
 
@@ -35,3 +55,4 @@ func (command Command) Run(args ...interface{}) (int, error) {
 
 	return -1, fmt.Errorf("there was no command to execute in the line [%s]", line)
 }
+
