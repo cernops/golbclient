680f1c93d90b344c7077e4f5c22afb4d88b9e2fc
Pablo Saiz <pablo.saiz@cern.ch>
2021-09-10 10:25:53 +0200

    Checking the scheduled reboots

--- lbconfig/lbconfig.go
+++ lbconfig/lbconfig.go
@@ -42,6 +42,7 @@ var allLBExpressions = map[string]ExpressionCode{
 	"CONSTANT":        {code: 16, cli: checks.MetricConstant{}},
 	"DAEMON":          {code: 17, cli: checks.DaemonListening{}},
 	"EOS":             {code: 18, cli: checks.EOS{}},
+	"REBOOT":          {code: 19, cli: checks.Reboot{}},
 	"XSESSIONS":       {code: 6, cli: checks.CheckAttribute{}},
 	"SWAPPING":        {code: 6, cli: checks.CheckAttribute{}},
 	"SWAPING":         {code: 6, cli: checks.CheckAttribute{}},
